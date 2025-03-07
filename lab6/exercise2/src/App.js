25c590fa614b2ac2290d8ba418fc863f54dbaa30
carson-fn <carson.fanai@ucalgary.ca>
2025-03-07 12:22:37 -0700

    Update App.js

--- lab6/exercise2/src/App.js
+++ lab6/exercise2/src/App.js
@@ -7,21 +7,21 @@ import Contact from './Contact.js';
 
 
 function App() {
-  let currentYear = new Date().getFullYear()
+  /*let currentYear = new Date().getFullYear()
   let isLoggedIn = true
   let logInMessage = ""
   if (isLoggedIn){
     logInMessage = "Welcome back!"
   } else {
     logInMessage = "Please log in."
-  }
+  }*/
 
   return (
     <div className="App">
-      <h1>ENSF-381: Full Stack Web Development</h1>
+    {/*<h1>ENSF-381: Full Stack Web Development</h1>
       <p>React Components</p>
       <p>{currentYear}</p>
-      <p>{logInMessage}</p>
+      <p>{logInMessage}</p>*/}
       <Home title="Home Page" description="Welcome to our website."/>
       <About title="About Us" description="We are passionate about delivering quality experiences."/>
       <Contact title="Contact Us" description="Feel free to reach out to us via email or phone."/>
